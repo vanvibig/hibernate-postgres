e3401b119978200d0daffaa71a267b47344c5262
nvvi <vanvibig@gmail.com>
2019-06-12 17:33:08 +0700

    ehcache success

--- src/main/java/com/example/hibernatepostgres/HibernatePostgresApplication.java
+++ src/main/java/com/example/hibernatepostgres/HibernatePostgresApplication.java
@@ -1,7 +1,13 @@
 package com.example.hibernatepostgres;
 
+import com.example.hibernatepostgres.service.MusicService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,10 +16,37 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing
 @EnableJpaRepositories
 @EnableCaching
-public class HibernatePostgresApplication {
+public class HibernatePostgresApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(HibernatePostgresApplication.class, args);
+    }
+
+    private static Logger log = LoggerFactory.getLogger(HibernatePostgresApplication.class);
+
+    @Autowired
+    private MusicService musicService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Spring Boot Ehcache 2 Caching Example Configuration");
+        log.info("using cache manager: " + cacheManager.getClass().getName());
+
+        musicService.clearCache();
+
+        play("trombone");
+        play("guitar");
+        play("trombone");
+        play("bass");
+        play("trombone");
+    }
+
+    private void play(String instrument){
+        log.info("Calling: " + MusicService.class.getSimpleName() + ".play(\"" + instrument + "\");");
+        musicService.play(instrument);
     }
 
 }
